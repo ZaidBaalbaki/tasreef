800fa330986653e54294a1a0ed7a31326caf9bb9
zaid baalbaki <zaidbaalbaki@Zaids-MacBook-Pro.local>
2025-01-23 11:04:09 +0200

    Remove Firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,9 @@ import 'package:easy_exchange/util/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(const EasyExchange());
 }
 
